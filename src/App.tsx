a18d51f1c9782583fafc70d570da6f96bd413237
Nelson Lago III <lagonelson77@gmail.com>
2026-07-22 15:31:17 +0800

    Added theme stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold text-blue-600">Tailwind works</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-primary bg-black">Tailwind works</h1>
+  );
 }

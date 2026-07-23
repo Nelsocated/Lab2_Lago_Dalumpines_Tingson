7399bfeadbd133e956bcba2e968f675d784cc482
Nelson Lago III <lagonelson77@gmail.com>
2026-07-23 12:20:57 +0800

    Fixed layout in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import FilterBar from "./components/FilterBar";
 
 export default function App() {
   return (
-    <main className="relative min-h-screen bg-white flex flex-col items-center text-black">
-      <>
-        <FilterBar />
-        <Header />
-      </>
-    </main>
+    <>
+      <Header />
+      <FilterBar />
+    </>
   );
 }

055ca98109e8be36920b19ce53e5d66b591cecb1
Nelson Lago III <lagonelson77@gmail.com>
2026-07-27 13:16:13 +0800

    Added filter logic and fixed cartmodal layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
 import FilterBar from "./components/FilterBar";
 import CartModal from "./components/CartModal";
+import type { Product } from "./components/interfaces";
+import { useState } from "react";
+
+const product: Product[] = [];
 
 export default function App() {
+  const [displayedProducts, setDisplayedProducts] =
+    useState<Product[]>(product);
+
   return (
     <main className="h-screen overflow-y-auto">
       <Header />
-      <FilterBar />
+      <FilterBar products={product} onFilter={setDisplayedProducts} />
 
       <CartModal />
     </main>
